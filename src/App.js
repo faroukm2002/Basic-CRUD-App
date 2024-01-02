ac94e0b6cd1b91c6f57a31ca36e5d4ea64e44971
faroukm2002 <faroukm238@gamil.com>
2024-01-03 01:51:39 +0200

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-
-   
+      <div className="row" >
+        <div className="col-2 sidebar" >
+          <Sidebar></Sidebar>
+        </div>
+        <div className="col-10">Home Page</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+function Sidebar(){
+
+return(
+    <>
+    <ul className="List-unstyled">
+    <li>
+        <a href="#">get all Products </a>
+    </li>
+    <li>
+        <a href="#">get all Categoies </a>
+    </li>
+    </ul>
+    
+    </>
+)
+
+}
+
+export default Sidebar
