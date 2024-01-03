246c472b62abc1cd8f869aac4a8e9fd584bcb7d2
faroukm2002 <faroukm238@gamil.com>
2024-01-03 03:32:03 +0200

    fetch data

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,35 +1,58 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-
+import "./products.css"
 function Products ()
 {
+const[products,setproduct] =useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:9000/products")
+        .then((res)=>res.json())
+         .then((data)=>{
+            console.log(data)
+         setproduct(data)       
+        })               
+        },[])
+        
 return (
+
     <>
     <h1>Products Page</h1>
 
        <Link to={'/products/add'}  className="btn btn-success mt-2">Add New Product </Link> 
-    <table class="table table-striped mt-5">
+    <table class="table table-striped mt-5 products-tabel">
     <thead>
+
         <tr>
             <th>ID</th>
             <th>Title</th>
+            <th>Description</th>
+
             <th>Price</th>
             <th>Operations</th>
 
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>1</td>
-            <td>product 1</td>
-            <td>100</td>
-            <td>
-            <button className="btn btn-danger btn-sm">Delete </button>
-            <button className="btn btn-info btn-sm">View </button>
-            <button className="btn btn-primary btn-sm">Edit </button>
+        {products.map((product) =>{
+            return (
+                <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.title}</td>
+                <td>{product.description.slice(0,20)}...</td>
 
-            </td>
-
-        </tr>
+                <td>{product.price}</td>
+                <td>
+                <button className="btn btn-danger btn-sm">Delete </button>
+                <button className="btn btn-info btn-sm">View </button>
+                <button className="btn btn-primary btn-sm">Edit </button>
+    
+                </td>
+    
+            </tr>
+            )
+        })}
+      
     </tbody>
 </table>
     </>
